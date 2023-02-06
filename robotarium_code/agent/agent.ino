b1240f8d59fe04520afcaf8e84a136ebe6742366
Jesús Chacón <jcsombria@gmail.com>
2023-02-06 12:49:25 +0100

    Encoders + Configurable H-Bridge

--- robotarium_code/agent/agent.ino
+++ robotarium_code/agent/agent.ino
@@ -1,186 +1,255 @@
-//robot2
-#include "common.h"
-#include <SimpleKalmanFilter.h>
+// Robotarium-UCM - Agent firmware
+// Copyright (C) 2022 UCM-237
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#include <MeanFilterLib.h>
 
 using namespace std;
-char packetBuffer[256]; //buffer to hold incoming packet
 
-//--------------------------------------------filtro de media movil simple para estabilizar la lectura de rad/s---------------------------------------
-MeanFilter<long> meanFilterD(10);
-MeanFilter<long> meanFilterI(10);
-//Time variables
-unsigned long previous_timer;
-unsigned long timer=10000;
+// H-BRIDGE - Uncomment only one option
+#define H_BRIDGE_RED 
+// #define H_BRIDGE_BLACK
 
+// -----------------------------------------------------------------------------
+// PIN layout
+// -----------------------------------------------------------------------------
+const int pin_left_encoder          = 2;
+const int pin_right_encoder         = 3;
 
-//operation variables
-/*struct appdata operation_send;
-struct appdata *server_operation;
-*/
+#ifdef H_BRIDGE_BLACK
+  const int pin_left_motor_direction  = 4;
+  const int pin_left_motor_enable     = 5;
+  const int pin_right_motor_direction = 8;
+  const int pin_right_motor_enable    = 9;
+#endif
 
-//prototypes//
+#ifdef H_BRIDGE_RED
+  const int pin_left_motor_dir_1   = 4;
+  const int pin_left_motor_dir_2   = 5;
+  const int pin_left_motor_enable  = 6;
+  const int pin_right_motor_dir_1  = 7;
+  const int pin_right_motor_dir_2  = 8;
+  const int pin_right_motor_enable = 9;
+#endif
 
-void motorSetup();
-void moveForward(const int pinMotor[3], int speed);
-void moveBackward(const int pinMotor[3], int speed);
-void fullStop(const int pinMotor[3]);
+// -----------------------------------------------------------------------------
+// Motors
+// -----------------------------------------------------------------------------
+const int LEFT_WHEEL = 0;
+const int RIGHT_WHEEL = 1;
+#define FORWARD LOW
+#define BACKWARD HIGH
+#define MINPWM 100
+#define MAXPWM 255
 
-void isrD();
-void isrI();
-int pidD(double wD);
-int pidI(double wI);
+void set_wheel_speed(int wheel, int direction, int speed) {
+#ifdef H_BRIDGE_BLACK
+  int pin_direction = (wheel == LEFT_WHEEL) ? pin_left_motor_direction : pin_right_motor_direction;
+  int dir = (wheel == LEFT_WHEEL) ? direction : !direction;
+  digitalWrite(pin_direction, dir);
+#endif
 
-void feedForwardD();
-void feedForwardI();
+#ifdef H_BRIDGE_RED
+  int pin_dir_1  = (wheel == LEFT_WHEEL) ? pin_left_motor_dir_1 : pin_right_motor_dir_2;
+  int pin_dir_2  = (wheel == LEFT_WHEEL) ? pin_left_motor_dir_2 : pin_right_motor_dir_1;
+  digitalWrite(pin_dir_1, (direction == FORWARD && speed != 0) ? HIGH : LOW);
+  digitalWrite(pin_dir_2, (direction == BACKWARD && speed != 0) ? HIGH : LOW);
+#endif
 
-int option;
-int led = LED_BUILTIN;
-
-void setup() {
-  motorSetup();
-  // Interrupciones para contar pulsos de encoder
-  pinMode(encoderD, INPUT_PULLUP);
-  pinMode(encoderI, INPUT_PULLUP);
-  pinMode(led,OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(encoderI), isrI, RISING);//prepara la entrada del encoder como interrupcion
-  attachInterrupt(digitalPinToInterrupt(encoderD), isrD, RISING);
-
-  // Se empieza con los motores parados
-  fullStop(pinMotorD);
-  fullStop(pinMotorI);
-
+  int pin_enable = (wheel == LEFT_WHEEL) ? pin_left_motor_enable : pin_right_motor_enable;
+  analogWrite(pin_enable, speed);
 }
-  
 
+// -----------------------------------------------------------------------------
+// Encoders
+// -----------------------------------------------------------------------------
+const int encoder_resolution_ppt = 20*2;
+int filter_window_size = 10;
+unsigned long encoder_count[2] = {0, 0};
+MeanFilter<double> encoder_w_estimated[2] = {
+  MeanFilter<double>(filter_window_size),
+  MeanFilter<double>(filter_window_size)
+};
+
+void isr_left_encoder_count() {
+  isr_encoder_count(LEFT_WHEEL);
+}
+
+void isr_right_encoder_count() {
+  isr_encoder_count(RIGHT_WHEEL);
+}
+
+void isr_encoder_count(int pin) {
+    encoder_count[pin] ++;
+}
+
+// -----------------------------------------------------------------------------
+// Setup
+// -----------------------------------------------------------------------------
+void setup() {
+  setup_motors();
+  setup_encoders();
+  setup_comms();
+}
+
+void setup_motors() {
+  pinMode(pin_left_motor_enable, OUTPUT);
+  pinMode(pin_right_motor_enable, OUTPUT);
+  #ifdef H_BRIDGE_RED
+    pinMode(pin_left_motor_dir_1, OUTPUT);
+    pinMode(pin_left_motor_dir_2, OUTPUT);
+    pinMode(pin_right_motor_dir_1, OUTPUT);
+    pinMode(pin_right_motor_dir_2, OUTPUT);
+  #endif
+  #ifdef H_BRIDGE_BLACK
+    pinMode(pin_left_motor_direction, OUTPUT);
+    pinMode(pin_right_motor_direction, OUTPUT);
+  #endif
+  set_wheel_speed(LEFT_WHEEL, FORWARD, 0);
+  set_wheel_speed(RIGHT_WHEEL, FORWARD, 0);
+}
+
+void setup_encoders() {
+  pinMode(pin_left_encoder, INPUT_PULLUP);
+  pinMode(pin_right_encoder, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), isr_left_encoder_count, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(3), isr_right_encoder_count, CHANGE);
+}
+
+void setup_comms() {
+  Serial.begin(9600);
+}
+// -----------------------------------------------------------------------------
+// Control Loop
+// -----------------------------------------------------------------------------
+const unsigned long sampling_time_us = 100000;
+unsigned long last_time_us = 0;
 
 void loop() {
-/*
- * VUESTRO CODIGO AQUI
- */
-}
-
-
-
-// Funcion que inicializa los pines de control de los motores de Arduino en modo salida
-void motorSetup() {
-  pinMode(pinIN1, OUTPUT);
-  pinMode(pinIN2, OUTPUT);
-  pinMode(pinENA, OUTPUT);
-  pinMode(pinIN3, OUTPUT);
-  pinMode(pinENB, OUTPUT);
-  pinMode(pinIN4, OUTPUT);
-}
-
-// Mueve hacia adelante el motor conectado a los pines pinMotor[2] con un valor de pwm indicado por speed
-// speed vale entre 0 y 255
-
-void moveForward(const int pinMotor[3], int speed) {
-  digitalWrite(pinMotor[1], HIGH);
-  digitalWrite(pinMotor[2], LOW);
-  analogWrite(pinMotor[0], speed);
-}
-
-// Mueve hacia atrás el motor conectado a los pines pinMotor[2] con un valor de pwm indicado por speed
-// speed vale entre 0 y 255
-
-void moveBackward(const int pinMotor[3], int speed) {
-  digitalWrite(pinMotor[1], LOW);
-  digitalWrite(pinMotor[2],HIGH);
-  analogWrite(pinMotor[0], speed);
-}
-
-// Para el motor conectado a los pines pinMotor[2]
-
-void fullStop(const int pinMotor[3]) {
-  digitalWrite(pinMotor[1], LOW);
-  digitalWrite(pinMotor[2],LOW);
-  analogWrite(pinMotor[0], 0);
-}
-
- 
-void isrD() {
-  timeBeforeDebounceD = millis();//tiempo para evitar rebotes
-  deltaDebounceD = timeBeforeDebounceD - timeAfterDebounceD;// tiempo que ha pasdo entre interrupcion e interrupcion
-  if(deltaDebounceD > TIMEDEBOUNCE) {//condicion para evitar rebotes
-    //se empieza a contar el tiempo que ha pasado entre una interrupcion "valida" y otra.    
-    startTimeD=micros();
-    encoder_countD++;
-      deltaTimeD = startTimeD - timeAfterD;
-      encoder_countD = 0;
-      timeAfterD = micros();
+  unsigned long current_time_us = micros();  
+  unsigned long dt_us = current_time_us - last_time_us; 
+  if(dt_us >= sampling_time_us) {
+    double w_left = encoder_w_estimated[LEFT_WHEEL].AddValue(encoder_count[LEFT_WHEEL]);
+    double w_right = encoder_w_estimated[RIGHT_WHEEL].AddValue(encoder_count[RIGHT_WHEEL]);
+    noInterrupts();
+    encoder_count[LEFT_WHEEL] = encoder_count[RIGHT_WHEEL] = 0;
+    interrupts();
+    update_control(w_left, w_right, dt_us*1e-6);
+    last_time_us = current_time_us;
   }
-  timeAfterDebounceD = millis();  
 }
 
-void isrI() {
-  timeBeforeDebounceI = millis();//tiempo para evitar rebotes
-  deltaDebounceI = timeBeforeDebounceI - timeAfterDebounceI;// tiempo que ha pasdo entre interrupcion e interrupcion
-  if(deltaDebounceI > TIMEDEBOUNCE) {//condicion para evitar rebotes
-    //se empieza a contar el tiempo que ha pasado entre una interrupcion "valida" y otra.    
-    startTimeI = micros();
-    encoder_countI++;//se cuenta los pasos de encoder
-    deltaTimeI = startTimeI - timeAfterI;
-    encoder_countI = 0;
-    timeAfterI = micros();
+// -----------------------------------------------------------------------------
+// PID Controller
+// -----------------------------------------------------------------------------
+unsigned long previous_time_ms[2] = {0, 0};
+double K_p[2] = {100, 100};
+double K_i[2] = {75, 75};
+double K_d[2] = {0.0, 0.0};
+double a_ff[2] = {1.8, 1.8/(MAXPWM - MINPWM)};
+double b_ff[2] = {1.8, 1.8/(MAXPWM - MINPWM)};
+double setpointW[2] = {3.0, 2.5}; // setpoint in { 0 U (1.8, 3.6) } 
+double setpoint_left_motor = 0;
+double setpoint_right_motor = 0;
+double previous_error[2] = {0, 0};
+double I_prev[2] = {0, 0};
+
+int pid_right_motor(double w) {
+  return pid(RIGHT_WHEEL, w);
+}
+
+int pid_left_motor(double w) {
+  return pid(LEFT_WHEEL, w);
+}
+
+int pid(int motor, double w) {
+  unsigned long current_time = millis();
+  double elapsed_time = current_time - previous_time_ms[motor];
+  double error = setpointW[motor] - w;
+  double u0 = feedforward(motor); 
+  double P = K_p[motor]*error;
+  double I = I_prev[motor] + K_i[motor]*error*elapsed_time*1e-3;
+  double D = K_d[motor]*(error - previous_error[motor]) / elapsed_time;
+  double u = u0 + P+I+D;
+  double v = constrain(u, MINPWM, MAXPWM);
+  if((u - v)*error <= 0) {
+    I_prev[motor] = I;
   }
-  timeAfterDebounceI = millis();
+  previous_error[motor] = error;
+  previous_time_ms[motor] = current_time;
+  return static_cast<int> (round(v));
 }
 
-int pidD(double wD) {
-  currentTimeD = millis();
-  elapsedTimeD = currentTimeD - previousTimeD;
-  int outputD = 0;
-  errorD = setpointWD - wD;
-  double aux = abs(errorD);
-  if(aux >= 0.30) {
-    cumErrorD += errorD * elapsedTimeD;                     // calcular la integral del error
-    if(lastErrorD > 0 && errorD < 0) { 
-       cumErrorD = errorD * elapsedTimeD;
-    }
-    if(lastErrorD < 0 && errorD > 0) {
-       cumErrorD = errorD * elapsedTimeD/1000;
-    }
-    constrain(cumErrorD, -MAXCUMERROR, MAXCUMERROR);
-    rateErrorD = (errorD - lastErrorD) / elapsedTimeD; // calcular la derivada del error
-    outputD = static_cast<int> (round(KD_p*errorD + KD_i*cumErrorD + KD_d*rateErrorD ));     // calcular la salida del PID    0.1*errorD +0.0065*cumErrorD + 0.0*rateErrorD
-    lastErrorD = errorD;                               // almacenar error anterior
-  }
-  previousTimeD=currentTimeD;
-  //Serial.println(errorD);//0.7*errorD + 0.06*cumErrorD + 0.0*rateErrorD 
-  //Serial.println(cumErrorD);
-  return outputD;
+int feedforward(int motor) {
+  return (setpointW[motor] != 0.0) ? constrain(
+    round((setpointW[motor] + a_ff[motor]) / b_ff[motor]), MINPWM, MAXPWM
+  ) : 0;    
 }
 
-int pidI(double wI) {
-  currentTimeI = millis();
-  elapsedTimeI = currentTimeI - previousTimeI;
-  int outputI = 0;
-  errorI = setpointWI - wI;   
-  double aux = abs(errorI);
-  if(aux >= 0.30) {
-    cumErrorI += errorI * elapsedTimeI; 
-    //se resetea el error acumulativo cuando se cambia de signo
-    if(lastErrorI>0 && errorI<0) {
-      cumErrorI = errorI * elapsedTimeI;
-    }
-    if(lastErrorI<0 && errorI>0) {
-      cumErrorI = errorI* elapsedTimeI/1000;
-    }
-    constrain(cumErrorI, -MAXCUMERROR, MAXCUMERROR);
-    rateErrorI = (errorI - lastErrorI) / elapsedTimeI; // calcular la derivada del error    
-    outputI = static_cast<int> (round(KI_p*errorI + KI_i*cumErrorI + KI_d*rateErrorI));     // calcular la salida del PID 0.42*errorI  + 0.006*cumErrorI + 0.00*rateErrorI
-    lastErrorI = errorI;
-  }
-  previousTimeI = currentTimeI;
-  return outputI;
-}
+// -----------------------------------------------------------------------------
+// INSTRUCCIONES
+// -----------------------------------------------------------------------------
+//   Inserta tu código en la función "update_control". Está función se invoca
+// períódicamente, pasándole información actualizada sobre el estado del robot.
+//
+// En ella puedes realizar llamadas a las funciones de la API para acceder a las
+// funciones básicas del robot:
+//   - set_wheel_speed(int wheel, int direction, int speed) 
+//
+// Si necesitas que el estado de una variable persista entre llamadas a
+// update_control, deberás utilizar una variable global. Utiliza la sección
+// comentada unas líneas más abajo. Para evitar conflictos de nombre con 
+// variables del firmware, utiliza el prefijo "user_". 
 
-void feedForwardD() {
-  PWM_D = (setpointWD != 0.0) ?
-    constrain(round((setpointWD - 0.0825) / 0.0707), MINPWM, MAXPWM) : 0;
-}
+// -----------------------------------------------------------------------------
+// Define aquí tus variables globales
+// -----------------------------------------------------------------------------
+// (Ej.: const int user_time = 0;)
 
-void feedForwardI(){
-  PWM_I = (setpointWI != 0.0) ? 
-    constrain(round((setpointWI + 1.656) / 0.0720), MINPWM, MAXPWM) : 0;
+// -----------------------------------------------------------------------------
+// update_control
+// -----------------------------------------------------------------------------
+//   Bucle de control principal. Ejecuta el código periódicamente, con un
+// periódo nominal especificado por el valor sampling_time_us.
+//
+// params
+// ------
+//   double w_left_wheel_rps  Velocidad angular de la rueda izquierda. 
+//   double w_right_wheel_rps Velocidad angular de la rueda derecha. 
+//   double dt_s              Indica el tiempo real, en segundos, transcurrido
+//                            desde la última invocación.
+// -----------------------------------------------------------------------------
+void update_control(double count_left_wheel, double count_right_wheel, double dt_s) {
+  double w_right_wheel = count_right_wheel / dt_s / encoder_resolution_ppt; // rps 
+  double w_left_wheel = count_left_wheel / dt_s / encoder_resolution_ppt; // rps 
+  double u_right = pid_right_motor(w_right_wheel); 
+  double u_left = pid_left_motor(w_left_wheel);
+  set_wheel_speed(LEFT_WHEEL, FORWARD, u_left);
+  set_wheel_speed(RIGHT_WHEEL, FORWARD, u_right);
+  Serial.print(" Period: ");
+  Serial.print(dt_s);
+  Serial.print(", Left wheel: ");
+  Serial.print(w_left_wheel);
+  Serial.print(", ");
+  Serial.print(encoder_count[LEFT_WHEEL]);
+  Serial.print(", Right wheel: ");
+  Serial.print(w_right_wheel);
+  Serial.print(", ");
+  Serial.print(encoder_count[RIGHT_WHEEL]);
+  Serial.print(", I[0]: ");
+  Serial.print(I_prev[0]);
+  Serial.print(", I[1]: ");
+  Serial.print(I_prev[1]);
+  Serial.println();
 }
